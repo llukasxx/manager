8339e5dca15fbd4732b62eb161f2feee1e4b61b3
Łukasz Gorzelany <llukasxx@gmail.com>
2017-05-01 16:21:45 +0200

    Add firebase auth

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDovbegdTbC9sPDSlaDdK0b9OvHZ7Lssu0',
+      authDomain: 'manager-c0f05.firebaseapp.com',
+      databaseURL: 'https://manager-c0f05.firebaseio.com',
+      projectId: 'manager-c0f05',
+      storageBucket: 'manager-c0f05.appspot.com',
+      messagingSenderId: '554990243236'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello
           </Text>
         </View>
       </Provider>
-    )
+    );
   }
 }
+
+export default App;
